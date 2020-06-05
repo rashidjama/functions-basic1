60cbf601da041793095d4777fbc63ab1937ea6e7
Rashid Jama <rashidjama5@gmail.com>
2020-06-05 09:16:17 -0700

    refactored my last challenge code

--- functions_intermediate2.py
+++ functions_intermediate2.py
@@ -72,19 +72,18 @@ students = [
 # Iterate Through a Dictionary with List Values
 # Create a function printInfo(some_dict) that given a dictionary whose values are all lists, prints the name of each key along with the size of its list, and then prints the associated values within each key's list. For example:
 
-# dojo = {
-#    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-#    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-# }
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
 
-# def printInfo(some_dict):
-#   print(len(some_dict['locations']), "LOCATIONS")
-#   for i in range(len(some_dict['locations'])):
-#     print(some_dict['locations'][i])
-#   print(len(some_dict['instructors']), 'Instructors')
-#   for i in range(len(some_dict['instructors'])):
-#     print(some_dict['instructors'][i])
-# printInfo(dojo)
+def printInfo(some_dict):
+  for key in some_dict.keys():
+    print(f"{len(some_dict[key])} {key.upper()}")
+    for item in some_dict[key]:
+      print(item)
+    print('\n')
+printInfo(dojo)
 
 # # output:
 # 7 LOCATIONS
@@ -105,3 +104,4 @@ students = [
 # Patrick
 # Minh
 # Devon
+
